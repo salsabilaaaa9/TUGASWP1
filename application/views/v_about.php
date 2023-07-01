bbcbca86a94ff2c68d6054bbd5313d34ef488fce
salsabilaaaa9 <amaliaslsabla@gmail.com>
2023-07-01 16:19:46 +0700

    TUGASWP1

---
+++ application/views/v_about.php
@@ -0,0 +1,20 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Amalia Salsabila</li>
+    </ul> 
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Rasamala III, Jakarta Selatan</li>
+    </ul>
+    <h4>Tempat/Tanggal Lahir</h4>
+    <ul type="none">
+        <li>12 September 2003</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+    </ul>
+</section>
